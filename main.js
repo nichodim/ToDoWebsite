77989ac171a10489cf1386766050302e2aabcfbd
nichodim <nickseagal321@gmail.com>
2022-11-29 21:50:54 -0500

    Implemented list creation

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function createToDo() {
-    
+    const txt = document.getElementById('textbox'); 
+    const l = document.createElement('li'); 
+
+    l.textContent = txt.value; 
+    l.setAttribute('class', 'list'); 
+
+    document.getElementById("myList").appendChild(l); 
 }
