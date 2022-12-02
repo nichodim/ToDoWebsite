7cdb44c6638bc34d49473535e51d88fc8d3793e0
nichodim <nickseagal321@gmail.com>
2022-12-02 18:13:23 -0500

    Implemented working checklist buttons
    Includes a button id system that connects button clicked and list item attached to function called. Also includes array for activation of buttons.

--- main.js
+++ main.js
@@ -3,25 +3,42 @@ function createToDo() {
     const l = document.createElement('li');
     const b = document.createElement('button'); 
 
-    const txtnos = txt.value.split(" ").join(""); 
-
     if (txt.value === "") {
         return; 
+    }
+    if (id == 0) {
+        id++; 
     }
 
     l.textContent = txt.value; 
     l.setAttribute('class', 'list'); 
 
-    b.setAttribute('id', txtnos); 
-    b.setAttribute('onclick', 'checkerClicked()'); 
+    b.setAttribute('onclick', `checkerClicked(${id})`); 
     b.setAttribute('class', 'checker'); 
+
+    l.setAttribute('id', `l${id}`); 
+    b.setAttribute('id', `b${id}`); 
+
+    btnActive.push(false); 
+    id++; 
 
     document.getElementById("myList").appendChild(l); 
     document.getElementById("buttondiv").appendChild(b); 
-    document.getElementById('textbox').value = "";
+    txt.value = "";
 }
 
-function checkerClicked() {
-    
-}
+function checkerClicked(idnum) {
+    const b = document.getElementById(`b${idnum}`); 
+    const l = document.getElementById(`l${idnum}`); 
 
+    if (btnActive[idnum] !== true) {
+        l.style.color = '#54c6ff'; 
+        b.style.backgroundColor = '#54c6ff'; 
+        btnActive[idnum] = true; 
+    }
+    else {
+        l.style.color = 'black'; 
+        b.style.backgroundColor = 'aliceblue'; 
+        btnActive[idnum] = false; 
+    }
+}
