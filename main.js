020de525e090cf4c320ece9318973b1064b417f3
nichodim <nickseagal321@gmail.com>
2022-12-07 19:25:57 -0500

    Added comments

--- main.js
+++ main.js
@@ -46,28 +46,39 @@ function createToDo() {
 }
 
 function saveState() {
-    saveState2(document.getElementsByClassName('listUA')); 
-    saveState2(document.getElementsByClassName('checkerUA')); 
-    saveState2(document.getElementsByClassName('listA')); 
-    saveState2(document.getElementsByClassName('checkerA')); 
+    // Send all existing todo items through saving process into localstorage
+    saveStateHTML(document.getElementsByClassName('listUA')); 
+    saveStateHTML(document.getElementsByClassName('checkerUA')); 
+    saveStateHTML(document.getElementsByClassName('listA')); 
+    saveStateHTML(document.getElementsByClassName('checkerA')); 
 
+    // Save current state of counters into localstorage
     localStorage.setItem("counters", JSON.stringify(counters)); 
 
 }
 
-function saveState2(objname) {
+function saveStateHTML(objname) {
+    // Recreate id with i in order to process every todo item
     for (let i = 0; i < objname.length; i++) {
+        // Remove deleted todo items from localstorage if detected
         if (ignoreThese[i] == (i + 1)) {
             localStorage.removeItem(`list${i + 1}`); 
             localStorage.removeItem(`button${i + 1}`); 
             return; 
         }
+
+        // Set placeholder object's variables to match important todo info
+        // Save the object with set variables with proper id number
+
+        // If list item
         if (objname[i].className.slice(0, 4) == 'list') {
             lists.id = objname[i].id; 
             lists.class = objname[i].className; 
             lists.text = objname[i].innerHTML; 
             localStorage.setItem(`list${i + 1}`, JSON.stringify(lists)); 
-        } else {
+        } 
+        // If button item
+        else {
             buttons.id = objname[i].id; 
             buttons.class = objname[i].className; 
             localStorage.setItem(`button${i + 1}`, JSON.stringify(buttons)); 
@@ -75,30 +86,30 @@ function saveState2(objname) {
     }
 }
 
+// Reset state of todo items from previous website uses
 function ResetState() {
+    // for every existing todo item in local storage...
     for (let i = 0; i < listArray.length; i++) {
         const l = document.createElement('li');
         const b = document.createElement('button'); 
         const lSaved = JSON.parse(localStorage.getItem(`list${i + 1}`))
         const bSaved = JSON.parse(localStorage.getItem(`button${i + 1}`))
 
-        if (ignoreThese[i + 1] != null) {
-            if (ignoreThese[i + 1].ignore) {
-                return; 
-            }
+        // If todo marked true in ignoreThese, skip
+        if (ignoreThese[i + 1] == null || !ignoreThese[i + 1].ignore) {
+            // Pipeline to set new element to old element and append
+            l.textContent = lSaved.text; 
+            l.setAttribute('class', lSaved.class); 
+
+            b.setAttribute('onclick', `checkerClicked(${i + 1})`); 
+            b.setAttribute('class', bSaved.class); 
+
+            l.setAttribute('id', lSaved.id); 
+            b.setAttribute('id', bSaved.id); 
+
+            document.getElementById("myList").appendChild(l); 
+            document.getElementById("buttondiv").appendChild(b); 
         }
-
-        l.textContent = lSaved.text; 
-        l.setAttribute('class', lSaved.class); 
-
-        b.setAttribute('onclick', `checkerClicked(${i + 1})`); 
-        b.setAttribute('class', bSaved.class); 
-
-        l.setAttribute('id', lSaved.id); 
-        b.setAttribute('id', bSaved.id); 
-
-        document.getElementById("myList").appendChild(l); 
-        document.getElementById("buttondiv").appendChild(b); 
 
         //document.getElementById("myList").append(JSON.parse(localStorage.getItem(`list${i + 1}`)));
         //document.getElementById("buttondiv").append(JSON.parse(localStorage.getItem(`button${i + 1}`)));
